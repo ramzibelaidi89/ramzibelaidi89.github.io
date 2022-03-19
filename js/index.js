bd8c81996d9d17ca3924a043e924aed341f4feb1
ramzibelaidi89 <77326718+ramzibelaidi89@users.noreply.github.com>
2022-03-19 16:13:42 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// lorem color 
+let h = document.getElementById('H').style.color = 'green';
+//  lorem color ends 
+
+function redrectmy(){
+    location.replace("https://www.facebook.com/ramzi.belaidi.7/")
+}
+
+// sticky navbar  
+// let navbar = document.getElementsByClassName('nav').style.position = 'sticky', top = 0 ;
+
+// sticky navbar ends 
+// append Supput us to the Nav bar  
+
+ const myList = document.getElementById('toDo'); 
+ const newLi = document.createElement('li'); 
+ newLi.textContent =  "Support "; 
+ myList.appendChild(newLi);
